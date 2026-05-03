2dc425cee5f893dd90413f2a0dc1ddba9d4a414b
siberett <126868806+siberett@users.noreply.github.com>
2026-05-03 22:31:35 +0200

    Mejora en los url paths

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -5,8 +5,8 @@ from urllib.request import urlopen
 
 import pytest
 
-BASE_URL = "http://localhost:5000"
-BASE_URL_MOCK = "http://localhost:9090"
+BASE_URL = os.getenv("APP_BASE_URL", "http://localhost:5001")
+BASE_URL_MOCK = os.getenv("WIREMOCK_BASE_URL", "http://localhost:8081")
 DEFAULT_TIMEOUT = 2  # in secs
 
 @pytest.mark.api
